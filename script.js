2dd944173c97167cae995d8e02d16f0a2791b817
Shahmir Hussain <shahmir.hussain.khanzada@gmail.com>
2021-11-15 19:26:36 +0500

    added dynamic behavior using Function

--- script.js
+++ script.js
@@ -1,138 +1,175 @@
 
-var students = [
+let students = [
     {
         fname: 'Shahmir',
         lname: 'hussain',
         age: 30,
-        designation: 'internee',
-        teacherName: 'Muhammad Furqan',
+        desig: 'internee',
+        teacherN: 'Muhammad Furqan',
         address: 'Karachi',
     },
     {
         fname: 'Muhammad',
         lname: 'Shaheem',
         age: 24,
-        designation: 'React Native Developer',
-        teacherName: 'Muhammad Farhan',
+        desig: 'React Native Developer',
+        teacherN: 'Muhammad Farhan',
         address: 'Karachi',
     },
     {
         fname: 'Muhammad',
         lname: 'Shaheer',
         age: 19,
-        designation: 'Student in Inter',
-        teacherName: 'Abdul Samad',
+        desig: 'Student in Inter',
+        teacherN: 'Abdul Samad',
         address: 'Karachi',
     },
     {
         fname: 'Muhammad',
         lname: 'Huzefa',
         age: 5,
-        designation: 'Student',
-        teacherName: 'Qari Sahab',
+        desig: 'Student',
+        teacherN: 'Qari Sahab',
         address: 'Karachi',
     },
     {
         fname: 'Abdul',
         lname: 'Hadi',
         age: 6,
-        designation: 'Student',
-        teacherName: 'Ali',
+        desig: 'Student',
+        teacherN: 'Ali',
         address: 'Karachi',
     },
     {
         fname: 'Anas',
         lname: 'Khanzada',
         age: 28,
-        designation: 'Civil Engineer',
-        teacherName: 'Muhammd Furqan',
+        desig: 'Civil Engineer',
+        teacherN: 'Muhammd Furqan',
         address: 'Karachi',
     },
     {
         fname: 'Ali',
         lname: 'Gabol',
         age: 21,
-        designation: 'UI Developer',
-        teacherName: 'Qasim',
+        desig: 'UI Developer',
+        teacherN: 'Qasim',
         address: 'Karachi',
     }
 
 ];
 
-var mainTable = document.getElementById('project');
 
-let table = document.createElement('table');
-table.classList.add('table', 'table-striped', 'table-hover');
-mainTable.appendChild(table);
+let officeEmployees = [
+    {
+        employee: 'Shoukat Memon',
+        Job: 'Executive Engineer',
+        age: '30',
+        Address: 'Tando Allahyar',   
+    },
+    {
+        employee: 'Naeem Zaidi',
+        job: 'Assistant Engineer',
+        age: '30',
+        Address: 'Tando Allahyar',
+    },
+    {
+        employee: 'Aijaz Malik',
+        job: 'Senior Engineer',
+        age: '30',
+        Address: 'Tando Allahyar',
+    },
+    {
+        employee: 'Muhammad Sajid',
+        job: 'Sub-Engineer',
+        age: '30',
+        Address: 'Tando Allahyar',
+    },
+    {
+        employee: 'Abdul Qadir',
+        job: 'Head Draightsman',
+        age: '30',
+        Address: 'Tando Allahyar',
+    },
+    {
+        employee: 'Abdul Jabbar',
+        job: 'Head Clerk',
+        age: '30',
+        Address: 'Tando Allahyar',
+    },
+];
 
 
-let tableHead = document.createElement('thead');
-table.appendChild(tableHead);
 
-let tableBody = document.createElement('tbody');
-table.appendChild(tableBody);
+let renderTable = (items, elementId) => {
 
+    let mainTable = document.getElementById(elementId);
 
-
-
-students.forEach((student, index) => {
-    let row = document.createElement('tr');
-    tableHead.appendChild(row);
-    tableBody.appendChild(row);
-
-
+    let table = document.createElement('table');
+    table.classList.add('table', 'table-striped', 'table-hover');
+    mainTable.appendChild(table);
     
-    if (index === 0) {
-        for(const key in student) {
-        console.log(`${key}: ${student[key]}`);
-        let heading = document.createElement('th');
-        heading.innerText = key.toUpperCase();
-        tableHead.appendChild(heading);
-    }
-}
-
-
-    for(const value in student) {
-        console.log(`${value}: ${student}`);
-        let column = document.createElement('td');
-        column.innerText = student[value];
-        row.appendChild(column);
-    }
-
-        //DELETE BUTTON//
-    let deleteButtonTd = document.createElement('td');
-    let deleteButton = document.createElement('button');
-    deleteButton.classList.add('btn', 'btn-danger');
-    deleteButton.innerText = 'Delete';
-    deleteButtonTd.appendChild(deleteButton);
-    row.appendChild(deleteButtonTd);
-
-    deleteButton.onclick = function () {
-        tableBody.removeChild(row);
-    }
-
-
-        //ADD BUTTON//
-    let addButtononTd = document.createElement('td');
-    let addButton = document.createElement('Button');
-    addButton.classList.add('btn', 'btn-primary');
-    addButton.innerText = 'Add';
-    addButtononTd.appendChild(addButton);
-    row.appendChild(addButtononTd);
-
-
-    addButton.onclick = function () {
+    
+    let tableHead = document.createElement('thead');
+    tableHead.classList.add('text-center');
+    table.appendChild(tableHead);
+    
+    let tableBody = document.createElement('tbody');
+    table.appendChild(tableBody);
+    
+    
+    
+    
+    items.forEach((student, index) => {
+        let row = document.createElement('tr');
+        tableHead.appendChild(row);
         tableBody.appendChild(row);
-    }
-
-
-
-
     
+    
+        
+        if (index === 0) {
+            for(const key in student) {
+            console.log(`${key}: ${student[key]}`);
+            let heading = document.createElement('th');
+            heading.innerText = key.toUpperCase();
+            tableHead.appendChild(heading);
+        }
+    }
+    
+    
+        for(const value in student) {
+            console.log(`${value}: ${student}`);
+            let column = document.createElement('td');
+            column.innerText = student[value];
+            row.appendChild(column);
+        }
+    
+            //DELETE BUTTON//
+        let deleteButtonTd = document.createElement('td');
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('btn', 'btn-danger');
+        deleteButton.innerText = 'Delete';
+        deleteButtonTd.appendChild(deleteButton);
+        row.appendChild(deleteButtonTd);
+    
+        deleteButton.onclick = function () {
+            tableBody.removeChild(row);
+        }
+    
+    
+            //ADD BUTTON//
+        let addButtononTd = document.createElement('td');
+        let addButton = document.createElement('Button');
+        addButton.classList.add('btn', 'btn-primary');
+        addButton.innerText = 'Add';
+        addButtononTd.appendChild(addButton);
+        row.appendChild(addButtononTd);
+    
+        addButton.onclick = function () {
+            tableBody.appendChild(row);
+        }
+    })   
+}
+renderTable(students, 'project');
 
-
-
-  
-   
-
-})
+renderTable(officeEmployees, 'testing');
